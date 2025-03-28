e39c41e28b02a4ce29a2b8930706a5646643fdc7
Groza Danut <grozadanut95@gmail.com>
2025-03-28 13:04:51 +0200

    Add Sentry(GlitchTip) exception logging

--- docker-push.bat
+++ docker-push.bat
@@ -1,4 +1,4 @@
-set VERSION=0.0.2
+set VERSION=0.0.3
 docker build -t grozadanut/legacy-cloud-acl:latest -t grozadanut/legacy-cloud-acl:%VERSION% --target production .
 docker push grozadanut/legacy-cloud-acl:%VERSION%
 docker push grozadanut/legacy-cloud-acl:latest
